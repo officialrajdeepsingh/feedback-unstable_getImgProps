7d93a236771ccdcc08e8dd7d862bb6a78734eade
officialrajdeepsingh <radewala.2@gmail.com>
2023-09-26 14:48:44 +0530

    update the header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,8 +3,8 @@ import React from "react";
 import { Logo } from "./logo";
 export const Header = () => {
   return (
-    <header className="text-gray-600 body-font">
-      <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+    <header className="container mx-auto flex flex-wrap py-5 flex-col md:flex-row items-center">
+    
         <Link
           href={"/"}
           className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0"
@@ -12,10 +12,9 @@ export const Header = () => {
           <Logo />
         </Link>
         <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
-          <Link href={"/"} className="mr-5 hover:text-gray-900">Home</Link>
-          <Link href={"/"} className="mr-5 hover:text-gray-900">About</Link>
+          <Link href={"/"} className="mr-5 text-black dark:text-white hover:text-gray-300">Home</Link>
+          <Link href={"/"} className="mr-5 text-black dark:text-white hover:text-gray-300">About</Link>
         </nav>
-      </div>
     </header>
   );
 };
